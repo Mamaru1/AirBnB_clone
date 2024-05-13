f0e5b9a8fff76d378aa4fc69d54dcc390d6f9d30
Mamaru1 <mamaru0920@gmail.com>
2024-05-13 12:00:14 -0700

    AirBnB project

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" method for models to excute each time when the package is colling"""
+
+from model.engin.file_storage import fileStorage
+
+
+storage=FileStorage()
+storage.relod()
